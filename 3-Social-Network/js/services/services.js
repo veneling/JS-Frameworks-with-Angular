a77ad5dadc32c230837f31f2448bbba9b4c5b172
veneling <veneling@users.noreply.github.com>
2015-05-25 21:53:28 +0300

    Delete services.js

--- 3-Social-Network/js/services/services.js
+++
@@ -1,9 +0,0 @@
-var services = angular.module('services', ['ngResource']);
-
-services.factory('loginService', ['$resource', '$rootScope', '$http',
-    function ($resource) {
-        return $resource('http://softuni-social-network.azurewebsites.net/api/users/login', {
-            username : 'veni', password : 'veniveni' }, {
-            query: {method:'GET'}
-            })
-    }]);
